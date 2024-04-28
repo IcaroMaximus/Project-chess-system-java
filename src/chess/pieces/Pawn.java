77d710cee08fe7229b006fcc01d9b28b0f7d2dcc
Icaro Maximus <icaromaximus3421@gmail.com>
2024-04-28 19:33:35 -0400

    En Passant ouui oui

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -10,7 +10,7 @@ public class Pawn extends ChessPiece {
 
 	private ChessMatch chessMatch;
 
-	public Pawn(Board board, Color color) {
+	public Pawn(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
 		this.chessMatch = chessMatch;
 	}
@@ -43,6 +43,19 @@ public class Pawn extends ChessPiece {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 
+			// #Specialmove en passant white
+			if (position.getRow() == 3) {
+				Position left = new Position(position.getRow(), position.getColumn() - 1);
+				if (getBoard().positionExists(left) && isThereOpponentPiece(left)
+						&& getBoard().piece(left) == chessMatch.getEnPassantVulnerable()) {
+					mat[left.getRow() - 1][left.getColumn()] = true;
+				}
+				Position right = new Position(position.getRow(), position.getColumn() + 1);
+				if (getBoard().positionExists(right) && isThereOpponentPiece(right)
+						&& getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
+					mat[right.getRow() - 1][right.getColumn()] = true;
+				}
+			}
 		}
 
 		else {
@@ -64,6 +77,20 @@ public class Pawn extends ChessPiece {
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
+			
+			// #Specialmove en passant black
+						if (position.getRow() == 4) {
+							Position left = new Position(position.getRow(), position.getColumn() - 1);
+							if (getBoard().positionExists(left) && isThereOpponentPiece(left)
+									&& getBoard().piece(left) == chessMatch.getEnPassantVulnerable()) {
+								mat[left.getRow() + 1][left.getColumn()] = true;
+							}
+							Position right = new Position(position.getRow(), position.getColumn() + 1);
+							if (getBoard().positionExists(right) && isThereOpponentPiece(right)
+									&& getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
+								mat[right.getRow() + 1][right.getColumn()] = true;
+							}
+						}
 		}
 		return mat;
 	}
